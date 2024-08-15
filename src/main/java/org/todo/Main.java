1838a0e41cda6dd253a0a7ff8b22f30c005f0d30
vhprestes <vhprestes@gmail.com>
2024-08-15 14:45:49 -0300

    fix: menu options

--- src/main/java/org/todo/Main.java
+++ src/main/java/org/todo/Main.java
@@ -1,5 +1,7 @@
 package org.todo;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -8,22 +10,23 @@ public class Main {
 
   public static void main(String[] args) {
 // TODO: método que lê o arquivo txt file.txt
+    File file = new File("file.txt");
     Scanner input = new Scanner(System.in);
     Menu();
     int option = input.nextInt();
 //    System.out.print("Você digitou o número:  " + option);
     switch (option){
       case 1:
-//        TODO: método que consulta as tasks concluidas no txt
-        System.out.println("Consultar tasks concluidas");
+//        TODO: método que consulta as tasks por status no txt
+        System.out.println("Consultar tasks por status");
         break;
       case 2:
-//        TODO: método que consulta as tasks sendo feitas no txt
-        System.out.println("Consultar tasks sendo feitas");
+//        TODO: método que consulta as tasks por prioridade no txt
+        System.out.println("Consultar tasks por prioridade");
         break;
       case 3:
-//        TODO: método que consulta as tasks a fazer no txt
-        System.out.println("Consultar tasks a fazer");
+//        TODO: método que consulta as tasks a por categoria no txt
+        System.out.println("Consultar tasks por categoria");
         break;
       case 4:
 //        TODO: método que adiciona uma task no txt
@@ -34,7 +37,7 @@ public class Main {
         System.out.println("Remover task");
         break;
       case 6:
-//        TODO: método que edita uma task no txt
+//        TODO: método que edita uma task no txt >> ESSE MÉTODO NÃO É OBRIGATÓRIO <<
         System.out.println("Editar task");
         break;
       case 7:
@@ -48,12 +51,15 @@ public class Main {
 
   public static void Menu(){
     System.out.println("Escolha uma opção:");
-    System.out.println("1- Consultar tasks concluidas");
-    System.out.println("2- Consultar tasks sendo feitas");
-    System.out.println("3- Consultar tasks a fazer");
+    System.out.println("1- Consultar tasks por status");
+    System.out.println("2- Consultar tasks por prioridade");
+    System.out.println("3- Consultar tasks por categoria");
     System.out.println("4- Adicionar task");
     System.out.println("5- Remover task");
     System.out.println("6- Editar task");
     System.out.println("7- Sair");
   }
+
+
+
 }
